9c1827a9969a3f882a8f0ea5e6a10dc23eb5ac92
Dylan Crowley <dcrowdev1025@gmail.com>
2022-11-01 17:22:07 -0400

    added seeds

--- db/schema.sql
+++ db/schema.sql
@@ -5,13 +5,13 @@ USE employer_db;
 
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
-    id INT NOT NULL,
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL
     department_id INT NOT NULL
@@ -19,8 +19,8 @@ CREATE TABLE role (
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employee (
-    id INT NOT NULL,
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+VALUES  ("Engineering"),
+        ("Finance"),
+        ("Legal"),
+        ("Sales");
+
+INSERT INTO roles (title, salary)
+VALUES  ("Sales Lead"),
+        ("Salesperson"),
+        ("Lead Engineer"),
+        ("Software Engineer"),
+        ("Account Manager"),
+        ("Accountant"),
+        ("Legal Team Lead"),
+        ("Lawyer");
+
+INSERT INTO employees (first_name, last_name)
+VALUES  ("John", "Doe"),
+        ("Mike", "Chan"),
+        ("Ashley", "Rodriguez"),
+        ("Kunal", "Singh"),
+        ("Malia", "Brown"),
+        ("Sarah", "Lourd"),
+        ("Tom", "Allen"),
+        ("Kevin", "Tupik");
