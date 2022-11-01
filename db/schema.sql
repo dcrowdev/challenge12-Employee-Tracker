8b0fd3fce3e1a2d7ddef54aba0eb8ec8c6c3cc98
Dylan Crowley <dcrowdev1025@gmail.com>
2022-11-01 17:48:05 -0400

    hardcoded seeds and schema working

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE employer_db;
 USE employer_db;
 
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30),
     PRIMARY KEY (id)
@@ -13,10 +13,10 @@ CREATE TABLE department (
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL
-    department_id INT NOT NULL
-    PRIMARY KEY (id)
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
@@ -24,8 +24,8 @@ CREATE TABLE employees (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL,
-    PRIMARY KEY (id)
-    FOREIGN KEY (role_id) REFERENCES role(id)
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
-)
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
