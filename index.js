45ed57ffab930318626f5e0b8f2906ee10bbf45e
Dylan Crowley <dcrowdev1025@gmail.com>
2022-11-02 17:46:37 -0400

    function setup

--- index.js
+++ index.js
@@ -48,11 +48,15 @@ const init = () => {
                 });
             } else if (answer.vieworadd === 'Add a department') {
                 addDepartment();
+            } else if (answer.vieworadd === 'Add a role') {
+                addRole();
+            } else if (answer.vieworadd === 'Add an employee') {
+                addEmployee();
+            } else if (answer.vieworadd === 'Update an employee role') {
+                updateEmployee();
             } 
-
-
-    })
-}
+    });
+};
 
 const addDepartment = () => {
     inquirer.prompt({
@@ -62,6 +66,42 @@ const addDepartment = () => {
 }).then(answer => {
     db.query(`INSERT INTO departments (name) VALUES ('${answer.department}')`)
 }).catch(err => console.error(err));
+}
+
+const addRole = () => {
+    let departments = [];
+    db.query(`SELECT * FROM departments`, (err, results) => {
+        if (err) {
+            throw err;
+        }
+    results.forEach(result => departments.push(result.name))  
+    console.log(departments)
+    })
+    inquirer.prompt({
+        type: 'input',
+        name: 'rolename',
+        message: 'What is the name of the role?'
+        },
+    {
+        type: 'input',
+        name: 'rolesalary',
+        message: 'What is the salary of the role?'
+    },
+    {
+        type: 'list',
+        name: 'roledepartment',
+        message: 'Which department does the role belong to?',
+        choices: [...departments]
+    }
+)
+}
+
+const addEmployee = () => {
+
+}
+
+const updateEmployee = () => {
+
 }
 
 
